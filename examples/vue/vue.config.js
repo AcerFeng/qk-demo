85b3ae7f2ccaab3fcc24d113c1126ddbfa2d29de
Leon <qinlanfeng@deepexi.com>
2021-05-12 14:38:06 +0800

    新增externals配置引入CDN资源

--- examples/vue/vue.config.js
+++ examples/vue/vue.config.js
@@ -35,29 +35,36 @@ module.exports = {
     },
   },
   configureWebpack: config => {
-    let plugins = []
-    if (process.env.NODE_ENV === 'production') {
-      // 为生产环境修改配置...
-      // 有几个 dll.js，这里就响应 new 几个 webpack.DllReferencePlugin
-      plugins = plugins.concat([
-        new webpack.DllReferencePlugin({
-          context: process.cwd(),
-          manifest: require('./public/vendor/vendor-manifest.json')
-        }),
-        // 将 dll 注入到 生成的 html 模板中
-        new AddAssetHtmlPlugin({
-          // dll文件位置
-          filepath: resolve('./public/vendor/*.js'),
-          // dll 引用路径
-          publicPath: './vendor',
-          // dll最终输出的目录
-          outputPath: './vendor'
-        })
-      ])
-    } else {
-      // 为开发环境修改配置...
-    }
-    config.plugins = [...config.plugins, ...plugins]
+    config.externals = {
+      vue: "Vue",
+      "element-ui": "ELEMENT",
+      "vue-router": "VueRouter",
+      vuex: "Vuex",
+      // axios: "axios"
+    };
+    // let plugins = []
+    // if (process.env.NODE_ENV === 'production') {
+    //   // 为生产环境修改配置...
+    //   // 有几个 dll.js，这里就响应 new 几个 webpack.DllReferencePlugin
+    //   plugins = plugins.concat([
+    //     new webpack.DllReferencePlugin({
+    //       context: process.cwd(),
+    //       manifest: require('./public/vendor/vendor-manifest.json')
+    //     }),
+    //     // 将 dll 注入到 生成的 html 模板中
+    //     new AddAssetHtmlPlugin({
+    //       // dll文件位置
+    //       filepath: resolve('./public/vendor/*.js'),
+    //       // dll 引用路径
+    //       publicPath: './vendor',
+    //       // dll最终输出的目录
+    //       outputPath: './vendor'
+    //     })
+    //   ])
+    // } else {
+    //   // 为开发环境修改配置...
+    // }
+    // config.plugins = [...config.plugins, ...plugins]
     Object.assign(config.resolve, {
       alias: {
         '@': resolve('src'),
@@ -69,5 +76,25 @@ module.exports = {
       libraryTarget: 'umd',
       jsonpFunction: `webpackJsonp_${name}`,
     })
+  },
+  chainWebpack: config => {
+    const cdn = {
+      // 访问https://unpkg.com/element-ui/lib/theme-chalk/index.css获取最新版本
+      css: ["//unpkg.com/element-ui@2.10.1/lib/theme-chalk/index.css"],
+      js: [
+        "//unpkg.com/vue@2.6.10/dist/vue.min.js", // 访问https://unpkg.com/vue/dist/vue.min.js获取最新版本
+        "//unpkg.com/vue-router@3.0.6/dist/vue-router.min.js",
+        "//unpkg.com/vuex@3.1.1/dist/vuex.min.js",
+        "//unpkg.com/axios@0.19.0/dist/axios.min.js",
+        "//unpkg.com/element-ui@2.10.1/lib/index.js"
+      ]
+    };
+
+    // 如果使用多页面打包，使用vue inspect --plugins查看html是否在结果数组中
+    config.plugin("html").tap(args => {
+      // html中添加cdn
+      args[0].cdn = cdn;
+      return args;
+    });
   }
 };
